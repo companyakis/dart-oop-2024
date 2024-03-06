8b3f7c481f86a02943b235e5fd794081f7fee1de
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-06 22:34:47 +0300

    Create A-OOP-002.dart

---
+++ A-OOP-002.dart
@@ -0,0 +1,27 @@
+// main.dart file
+
+import 'class.dart';
+
+void main() {
+  //create object
+
+  var opel = Car(color: "White", speed: 100, isRunning: true);
+
+  opel.giveInfo(); // Car color: White, car speed: 100, is Car running: true
+
+  opel.accelerate(25);
+
+  opel.giveInfo(); // Car color: White, car speed: 125, is Car running: true
+
+  opel.decelerate(15);
+
+  opel.giveInfo(); // Car color: White, car speed: 110, is Car running: true
+
+  opel.stopIt();
+
+  opel.giveInfo(); //Car color: White, car speed: 0, is Car running: false
+
+  opel.startIt();
+
+  opel.giveInfo(); //Car color: White, car speed: 10, is Car running: true
+}
