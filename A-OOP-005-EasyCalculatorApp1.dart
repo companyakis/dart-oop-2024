65a2d72a654c32b655a27de4cda92213a802e68c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-07 09:00:20 +0300

    Create A-OOP-005-EasyCalculatorApp1.dart

---
+++ A-OOP-005-EasyCalculatorApp1.dart
@@ -0,0 +1,31 @@
+//easycalculator.dart file
+
+class EasyCalculator {
+  late double x;
+  late double y;
+
+  double addition(x, y) {
+    return x + y;
+  }
+
+  double subtraction(x, y) {
+    return x - y;
+  }
+
+  double multiplication(x, y) {
+    return x * y;
+  }
+
+  double division(x, y) {
+    return x / y;
+  }
+
+  double square(x) {
+    return x * x;
+  }
+
+  double cube(x) {
+    return x * x * x;
+  }
+
+}
